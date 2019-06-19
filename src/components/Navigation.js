a5069ad6b852ed53d891ad1ceddf487690e174c5
teej387 <teej387@yahoo.com>
2019-06-19 18:34:50 -0500

    Set route paths for nav components and set to links

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Route, Link} from 'react-router-dom';
-import {Home, About, Contact} from '../Components';
+import {Home, About, Contact} from '../components';
 
 const Navigation = () => {
   return (
@@ -9,15 +9,19 @@ const Navigation = () => {
         <nav className='navBar'>
           <h1>React Router Mini</h1>
           <div>
-            <a href="">Home</a>
+            <Link to='/'>Home</Link>
           </div>
           <div>
-            <a href="">About</a>
+            <Link to='/about'>About</Link>
           </div>
           <div>
-            <a href="">Contact</a>
+            <Link to='/contact'>Contact</Link>
           </div>
         </nav>
+
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
       </div>
     </div>
   );
