ecca7aa0718a75d679862cafd6e9f3b300dcab9c
teej387 <teej387@yahoo.com>
2019-06-19 19:35:33 -0500

    Styling tweaks

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,22 +3,22 @@ import {Link} from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <div>
       <div className="App">
         <nav className='navBar'>
           <h1>React Router Mini</h1>
-          <div>
-            <Link to='/'>Home</Link>
-          </div>
-          <div>
-            <Link to='/about'>About</Link>
-          </div>
-          <div>
-            <Link to='/contact'>Contact</Link>
+          <div className='navLinks'>
+            <div>
+            <Link className='link' to='/'>Home</Link>
+            </div>
+            <div>
+            <Link className='link' to='/about'>About</Link>
+            </div>
+            <div>
+              <Link className='link' to='/contact'>Contact</Link>
+            </div>
           </div>
         </nav>
       </div>
-    </div>
   );
 };
 
